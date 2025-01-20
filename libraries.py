eade56a2436ef4edb9c5e8c6045a332a1b4e2a83
Mohsin Ali <mohsinali@Mohsins-MacBook-Pro.local>
2025-01-20 22:17:51 +0500

    Adding the automation file

---
+++ libraries.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import base64
+import time
