0659921cce8c5f1e5dcbc4aa5cacbb2a4244d05a
Mohsin Ali <mohsin.ali3065@gmail.com>
2025-01-21 13:13:29 +0500

    git changes

--- passwords.py
+++ passwords.py
@@ -1,15 +1,18 @@
 import base64
 
+#Function to encode the given values
 def encoded_password(value):
     string = value.encode('utf-8')
     encoded_bytes = base64.b64encode(string)
     return encoded_bytes
 
+#Function to decode the output of the encoded values
 def decoded_password(encoded_bytes):
     string = encoded_bytes.decode('utf-8')
     decoded_password = base64.b64decode(string)
     return decoded_password
 
+#Conversion of the values
 def instances(values):
     if isinstance(values,bytes):
         values = values.decode('utf-8')
@@ -17,17 +20,8 @@ def instances(values):
         values = str(values)
     return values
 
-user_name = 'admin'
-password = 'Admin@78666'
 
-# def encode_password(password):
-#     password_bytes = password.encode('utf-8')  # Convert string to bytes
-#     encoded_bytes = base64.b64encode(password_bytes)  # Encode the bytes
-#     encoded_password = encoded_bytes.decode('utf-8')  # Convert bytes back to string
-#     return encoded_password
+user_name = ''
+password = ''
 
-# def decode_password(encoded_password):
-#     encoded_bytes = encoded_password.encode('utf-8')  # Convert string to bytes
-#     decoded_bytes = base64.b64decode(encoded_bytes)  # Decode the bytes
-#     decoded_password = decoded_bytes.decode('utf-8')  # Convert bytes back to string
-#     return decoded_password
+
